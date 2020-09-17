61ab84aad101f324d06fcd9a8031742b43b03941
nahid <nahidkishore99@gmail.com>
2020-09-18 02:48:15 +0600

    project finished

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,28 +20,31 @@ const Header = () => {
   };
   return (
     <div>
-      <Navbar bg="success" variant="dark" className="">
-    <Navbar.Brand  href=""><Link to="/home"><img style={{ width: "10%",height: "10%"}} className="brand-logo" src="https://iili.io/2xWwrJ.png" alt="" /></Link></Navbar.Brand>
+      <Navbar  bg="success" expand="md" variant="dark">
+    <Navbar.Brand  href=""><Link to="/"><img style={{ width: "10%",height: "10%"}} className="brand-logo" src="https://iili.io/2xWwrJ.png" alt="" /></Link></Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
     <Form inline>
-      <FormControl type="text" placeholder="Search your Destination..." className="mr-sm-2" />
+      <FormControl  type="text" placeholder="Search your Destination..." className="mr-sm-2" />
      
     </Form>
     <Nav className="mr-auto nav-header">
-      <Nav.Link href=""><Link to="/news">News</Link></Nav.Link>
+      <Nav.Link href="" style={{ fontWeight: "0",fontSize: "32px" }}><Link to="/news">News</Link></Nav.Link>
       {/* <Nav.Link href="/"><Link to="/destination">Destination</Link> </Nav.Link> */}
-      <Nav.Link href=""><Link to="/blog">Blog</Link></Nav.Link>
-      <Nav.Link href=""><Link to="/contact">Contact</Link></Nav.Link>
+      <Nav.Link href="" style={{ fontWeight: "0",fontSize: "32px" }}><Link to="/blog">Blog</Link></Nav.Link>
+
+      <Nav.Link href="" style={{ fontWeight: "0",fontSize: "32px" }}><Link to="/contact">Contact</Link></Nav.Link>
+
       
-  <p style={{color: 'yellow'}}> {loggedInUser.displayName}</p>
+      <Nav.Link href=""><p style={{color: 'yellow', weight: 'normal'}}> {loggedInUser.displayName}</p></Nav.Link>
+  
       
     </Nav>
     {/* <Link to="/login"><Button>Login</Button></Link> */}
-    <Nav.Link onClick={handleLoggingButton}  href="" > <Button> {loggedInUser.name ? "LogOut" : "Login"}</Button>
+    <Nav.Link onClick={handleLoggingButton}  href="" > <Button  variant="success"> {loggedInUser.name ? "LogOut" : "Login"}</Button>
            
           </Nav.Link>
-    {/* <Button onClick={()=> setLoggedInUser({})} variant="warning">Sign Out</Button> */}
+   
     </Navbar.Collapse>
     
   
